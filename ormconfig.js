74c943ecde59218319782746f5b888cf0c8ee251
AvroraPolnareff <AraInTheSkyWith@gmail.com>
2021-03-14 13:56:27 +0300

    add ssl unauthorized

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,9 @@ const path = require("path");
 module.exports = {
     type: "postgres",
     url: process.env.DATABASE_URL,
-    ssl: true,
+    ssl: {
+        rejectUnauthorized: false
+    },
     synchronize: true,
     "entities": [
         path.join(__dirname, "/src/db/entity/**/*{.ts,.js}")
